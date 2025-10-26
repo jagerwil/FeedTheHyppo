afc9bffd2567f5f60c0b966be5a4fbe61d7f37b5
jagerwil <31615013+jagerwil@users.noreply.github.com>
2025-10-26 17:28:53 +0300

    Added interaction crosshair when can grab melon

---
+++ Assets/Scripts/Gameplay/UI/Crosshair.cs
@@ -0,0 +1,29 @@
+using System;
+using FeedTheHyppo.Gameplay._Providers;
+using FeedTheHyppo.Gameplay.Items;
+using R3;
+using UnityEngine;
+using Zenject;
+
+namespace FeedTheHyppo.Gameplay.UI {
+    public class Crosshair : MonoBehaviour {
+        [SerializeField] private GameObject _defaultCrosshair;
+        [SerializeField] private GameObject _interactCrosshair;
+        
+        [Inject] private IPlayerItemInteractionProvider _itemInteractionProvider;
+
+        private readonly CompositeDisposable _disposables = new();
+
+        private void Start() {
+            _itemInteractionProvider.LookedAtItem.Subscribe(LookedAtItemChanged)
+                                                 .AddTo(_disposables);
+        }
+
+        private void LookedAtItemChanged(BaseItem item) {
+            var hasInteraction = (bool)item;
+            
+            _defaultCrosshair.SetActive(!hasInteraction);
+            _interactCrosshair.SetActive(hasInteraction);
+        }
+    }
+}
