f21a622ec188e230bdc18a40d5c1f16f9e80789f
jagerwil <31615013+jagerwil@users.noreply.github.com>
2025-10-28 12:11:16 +0300

    Despawn all particles when restarting the scene

--- Assets/Scripts/Architecture/StateMachine/Gameplay/GameplayRestartState.cs
+++ Assets/Scripts/Architecture/StateMachine/Gameplay/GameplayRestartState.cs
@@ -1,3 +1,4 @@
+using FeedTheHyppo.Architecture._Factories;
 using FeedTheHyppo.Gameplay._Factories;
 using FeedTheHyppo.Gameplay._Providers;
 using Jagerwil.Core.Architecture.StateMachine;
@@ -8,19 +9,23 @@ namespace FeedTheHyppo.Architecture.StateMachine.Gameplay {
         private readonly IGameStateMachine _stateMachine;
         private readonly IPlayerFactory _playerFactory;
         private readonly IFoodItemFactory _foodItemFactory;
+        private readonly IParticleSystemFactory _particlesFactory;
         private readonly ISceneObjectsProvider _sceneObjectsProvider;
 
         public GameplayRestartState(IGameStateMachine stateMachine,
             IPlayerFactory playerFactory,
-            IFoodItemFactory foodItemFactory) {
+            IFoodItemFactory foodItemFactory,
+            IParticleSystemFactory particlesFactory) {
             _stateMachine = stateMachine;
             _playerFactory = playerFactory;
             _foodItemFactory = foodItemFactory;
+            _particlesFactory = particlesFactory;
         }
         
         public void Enter() {
             _playerFactory.DespawnAll();
             _foodItemFactory.DespawnAll();
+            _particlesFactory.DespawnAll();
 
             _stateMachine.Enter<GameplayMainState>();
         }
